4917d20e5985ec6f31117834d4eceaa4fb069086
Jack Alder <jack@jalder.com>
2011-05-07 12:43:05 -0400

    Missing File

---
+++ includes/languages/english/modules/payment/bitcoin.php
@@ -0,0 +1,8 @@
+<?php
+//	Bitcoin Payment Module - Lanugage File
+//
+
+  define('MODULE_PAYMENT_BITCOIN_TEXT_TITLE', 'Bitcoin Payment');
+  define('MODULE_PAYMENT_BITCOIN_TEXT_DESCRIPTION', 'Collect payments using bitcoin p2p network.');
+  define('MODULE_PAYMENT_BITCOIN_TEXT_EMAIL_FOOTER', 'Payed with Bitcoins - Thanks for the support!');
+?>
