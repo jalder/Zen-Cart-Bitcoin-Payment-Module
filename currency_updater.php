7c0bbe63949b8e62f83074df61cd9f160f03c723
MasterX1582 <davy1582@msn.com>
2013-07-20 12:01:23 +0200

    Update currency_updater.php
    added support for the btce api.

--- currency_updater.php
+++ currency_updater.php
@@ -1,37 +1,34 @@
+#!/usr/bin/php
 <?php
-/**
- *
- * Bitcoin Payment Module
- * 
- * @package paymentMethod
- *
- *  Author: Adiow (https://github.com/adiow/Zen-Cart-Bitcoin-Payment-Module/)
- *  Moddified by:  MasterX1582 (https://github.com/MasterX1582/Zen-Cart-Bitcoin-Payment-Module/)
- *  Donations: 1JBKYhNvF1B8eLEcCUq3jw8wvrzDCPCGiB
- *
- **/
+ 
 
 #Script patched together from snippets of code I found on the web. Adrian Golser, Sept. 2012.
-#Enter defining data at beginning, make sure mtgox-api returns correct value and set up a cron-job to execute
+#Enter defining data at beginning, make sure btce-api returns correct value and set up a cron-job to execute
 #according to preferences.
+
+define('BTCEXCHANGE', 'btce'); 	// Wich Exchange are we using? 
+				// For MtGox use code 'mtgox'.
+				// For BTC-E use code 'btce'.
 
 
 
 # Set this to the physical path of your installation.
 # Make sure to include the trailing "/"
-define('PATH_TO_STORE_ROOT','/home/user/zen/');
+define('PATH_TO_STORE_ROOT','/var/zpanel/hostdata/megax/public_html/electro_megaxnetwork_com/');
 
 
-#Settings for rate calculation: (typical mtgox-api-output: {"ticker":{"high":8.79,"low":8.40145,"avg":8.583123374,"vwap":8.56141034,"vol":2342,"last_all":8.679,"last_local":8.679,"last":8.679,"buy":8.59688,"sell":8.679}}
+#Settings for rate calculation: (typical btce-api-output: {"ticker":{"high":8.79,"low":8.40145,"avg":8.583123374,"vwap":8.56141034,"vol":2342,"last_all":8.679,"last_local":8.679,"last":8.679,"buy":8.59688,"sell":8.679}}
 define('WHICH_VALUE','avg');
-define('EXCHANGE_FEE','1.02'); //Exchange fee you add to your rate. This is just multiplied, for 2% write 1.02
-define('DEFAULT_SYMBOL','EUR');
 
+define('DEFAULT_SYMBOL','USD');
 
-#API-Settings. Create from your mtgox-account.
-define('MTGOX_KEY','');
-define('MTGOX_SECRET','');
-
+if (BTCEXCHANGE != 'btce'){ // For BTC-E we get the fee from the api so this is not needed.
+	define('EXCHANGE_FEE','02'); // Exchange fee you add to your rate. Example: write 02 for 2%
+	
+	#API-Settings. Create from your mtgox-account.
+	define('MTGOX_KEY','');
+	define('MTGOX_SECRET','');
+}
 
 /////////////////////
 
@@ -42,8 +39,8 @@ require_once(PATH_TO_STORE_ROOT . 'includes/configure.php');
 require_once(PATH_TO_STORE_ROOT . 'includes/database_tables.php');
 
 
-//get ticker data from mtgox. Code is from bitcoin wiki. Output is 2d-array
-function mtgox_query($path, array $req = array()) {
+//get ticker data from btce. Code is from bitcoin wiki. Output is 2d-array
+function exchange_query($path, array $req = array()) {
   // API settings
 
  
@@ -54,12 +51,14 @@ function mtgox_query($path, array $req = array()) {
 	// generate the POST data string
 	$post_data = http_build_query($req, '', '&');
  
-	// generate the extra headers
-	$headers = array(
-		'Rest-Key: '.MTGOX_KEY,
-		'Rest-Sign: '.base64_encode(hash_hmac('sha512', $post_data, base64_decode(MTGOX_SECRET), true)),
-	);
- 
+	if (BTCEXCHANGE != 'btce'){
+		// generate the extra headers
+		$headers = array(
+			'Rest-Key: '.MTGOX,
+			'Rest-Sign: '.base64_encode(hash_hmac('sha512', $post_data, base64_decode(MTGOX_SECRET), true)),
+		);
+	}
+
 	// our curl handle (initialize if required)
 	static $ch = null;
 	if (is_null($ch)) {
@@ -67,9 +66,11 @@ function mtgox_query($path, array $req = array()) {
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MtGox PHP client; '.php_uname('s').'; PHP/'.phpversion().')');
 	}
-	curl_setopt($ch, CURLOPT_URL, 'https://data.mtgox.com/api/'.$path);
+	
+	if (BTCEXCHANGE == 'btce'){ curl_setopt($ch, CURLOPT_URL, 'https://btc-e.com/api/2/'.$path);}
+	if (BTCEXCHANGE == 'mtgox'){ curl_setopt($ch, CURLOPT_URL, 'https://data.mtgox.com/api/'.$path);}
 	curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
-	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+	if (BTCEXCHANGE != 'btce'){curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);}
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
  
 	// run the query
@@ -82,14 +83,21 @@ function mtgox_query($path, array $req = array()) {
  
 
 //bit of math
-
-/* Note: "warning":"THIS API IS DEPRECATED. Please upgrade to newer API such as https:\/\/data.mtgox.com\/api\/2\/BTCUSD\/money\/ticker" */
-$mtgox_array = mtgox_query('0/ticker.php?Currency='.DEFAULT_SYMBOL);
-$newrate = (1 / (($mtgox_array["ticker"][WHICH_VALUE]) * EXCHANGE_FEE));
+if (BTCEXCHANGE == 'mtgox'){
+	define('EFEE','1.'.EXCHANGE_FEE); // No need to change this...
+	$exchange_array = exchange_query('0/ticker.php?Currency='.DEFAULT_SYMBOL);
+}elseif (BTCEXCHANGE == 'btce'){
+	$feeurl = 'https://btc-e.com/api/2/btc_'.strtolower(DEFAULT_SYMBOL).'/fee';
+	$json = file_get_contents($feeurl); 
+	$fee = json_decode($json);
+	define('EFEE', '1.'.($fee->trade * 100)); 
+	$exchange_array = exchange_query('btc_'.strtolower(DEFAULT_SYMBOL).'/ticker');
+}
+$newrate = (1 / (($exchange_array["ticker"][WHICH_VALUE]) * EFEE));
 
 //uncomment for testing
-//return (var_dump((float)($mtgox_array["ticker"][WHICH_VALUE])));
-//return (var_dump($newrate));
+return (var_dump((float)($exchange_array["ticker"][WHICH_VALUE])));
+return (var_dump($newrate));
 
 
 //Time to punch this into the database
